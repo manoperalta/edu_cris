f8abf31ecbfc9200617d06e97d5939568d82e1d6
Jonathas Martinelli Peralta <63939605+manoperalta@users.noreply.github.com>
2023-03-12 12:10:23 -0300

    versao rodando
    Essa versao esta rodando no site

--- footer.php
+++ footer.php
@@ -1,9 +1,41 @@
+<div class="container-fluid bg-dark" style="padding: 15px;">
+  <div class="row">
+    <div class="col">
+      <p class="text-center" style="font-size: medium;color: #ffffff;">Links</p>
+    <div class="container">
+  <div class="row">
+    <div class="col">
+      
+    </div>
+    <div class="col text-center">
+      <a href="cadastrar.php">cadastro</a><br>
+      <a href="financeiro.php">financeiro</a><br>
+      <a href="editar.php">editar</a><br>
+      <a href="busca.php">buscar</a><br>
+    </div>
+    <div class="col">
+      
+    </div>
+  </div>
+</div>
+    </div>
+    <div class="col text-center">
+      
+    <img class="img-fluid" src="img/logo.png" alt="Espaco da Prof Cris" width="257" height="87">
 
+
+    </div>
+    <div class="col text-center">
+    <img class="img-fluid" src="img/seguranca.png" alt="100% Seguro!" width="57" height="17">  
+<p class="text-center" style="color:#ffffff;">Segurança 100%</p>
+    </div>
+  </div>
+</div>
 <div class="container-fluid bg-dark">
 <div class="container text-center">
   <div class="row">
         <div class="col text-light">
-      by Jônathas Martinelli Peralta
+      <p class="text-center m-auto" style="color:#ffffff;"> Jônathas Martinelli Peralta</p>
     </div>
     
   </div>
--- login.sql
+++ login.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Tempo de geração: 22/01/2023 às 20:14
+-- Tempo de geração: 12/03/2023 às 15:52
 -- Versão do servidor: 10.4.27-MariaDB
 -- Versão do PHP: 8.1.12
 
@@ -44,18 +44,17 @@ CREATE TABLE `usuarios` (
   `financeiro_pago` varchar(500) NOT NULL,
   `valor_aula` varchar(20) NOT NULL,
   `divida_ativa` varchar(20) NOT NULL,
-  `divida_paga` varchar(20) NOT NULL
+  `divida_paga` varchar(20) NOT NULL,
+  `reg_aula` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Despejando dados para a tabela `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nome`, `fone`, `responsavel`, `endereco`, `email`, `data_nasc`, `tipo`, `agenda`, `senha`, `aulas`, `matricula`, `financeiro_aberto`, `financeiro_pago`, `valor_aula`, `divida_ativa`, `divida_paga`) VALUES
-(1, 'Mano', '51981717498', 'Jonathas', 'Rua Capitao Cruz', 'mano@mano', '0000-00-00', '', 'segunda-feira: 8h<br>terça-feira: 9h<br>quarta-feira: 15h<br>quinta-feira: 17h', '123', ' | Domingo, 22 de Janeiro de 2023 : 0001 - aberto | Domingo, 22 de Janeiro de 2023 : 0002 Pago | Domingo, 22 de Janeiro de 2023 : 003 aberto | Domingo, 22 de Janeiro de 2023 : 0004 pago | Domingo, 22 de Janeiro de 2023 : 005 pago | Domingo, 22 de Janeiro de 2023 : 0006 aberto | Domingo, 22 de Janeiro de 2023 :  | Domingo, 22 de Janeiro de 2023 :  | Domingo, 22 de Janeiro de 2023 : 0098 | <p>Domingo, 22 de Janeiro de 2023 : <br>00010<p>', '0', 'Pagou: R$:10Domingo, 22 de Janeiro de 2023 : ', '0Domingo, 22 de Janeiro de 2023 : Domingo, 22 de Janeiro de 2023 : Domingo, 22 de Janeiro de 2023 : Domingo, 22 de Janeiro de 2023 : Domingo, 22 de Janeiro de 2023 : Domingo, 22 de Janeiro de 2023 : Domingo, 22 de Janeiro de 2023 : Domingo, 22 de Janeiro de 2023 : ', '30', '0', '300'),
-(32, 'krika', '5165363187', 'Adão Negro', 'Rua José Luiz, N20', 'krika@krika', '1990-08-19', 'aluno', '<br><br><br>', '123', '[Data da Matrícula: Domingo 22 de Janeiro de 2023] | Domingo, 22 de Janeiro de 2023 : 001 - Aberto | Domingo, 22 de Janeiro de 2023 : 002 - Aberto | Domingo, 22 de Janeiro de 2023 : 003 - Pago | Domingo, 22 de Janeiro de 2023 : 0001 | Domingo, 22 de Janeiro de 2023 : 0002 | Domingo, 22 de Janeiro de 2023 : 0011444 | Domingo, 22 de Janeiro de 2023 : 05555 | Domingo, 22 de Janeiro de 2023 : 0006 | Domingo, 22 de Janeiro de 2023 : 0007 | Domingo, 22 de Janeiro de 2023 : 0008 | Domingo, 22 de Janeiro de 2023 : 0009 | Domingo, 22 de Janeiro de 2023 : 00010 | Domingo, 22 de Janeiro de 2023 : 00111 | Domingo, 22 de Janeiro de 2023 : 0012 | Domingo, 22 de Janeiro de 2023 : 0013 | Domingo, 22 de Janeiro de 2023 : 000014 | Domingo, 22 de Janeiro de 2023 : 0015 | Domingo, 22 de Janeiro de 2023 : 0016 | Domingo, 22 de Janeiro de 2023 : 0007 | <p>Domingo, 22 de Janeiro de 2023 : <br>0018<p>', 'Domingo 22 de Janeiro de 2023', 'Pagou: R$:30Domingo, 22 de Janeiro de 2023 : Domingo, 22 de Janeiro de 2023 : Domingo, 22 de Janeiro de 2023 : Domingo, 22 de Janeiro de 2023 : Domingo, 22 de Janeiro de 2023 : Domingo, 22 de Janeiro de 2023 : ', 'Pagamento: Domingo, 22 de Janeiro de 2023 : Domingo, 22 de Janeiro de 2023 : Domingo, 22 de Janeiro de 2023 : Domingo, 22 de Janeiro de 2023 : Domingo, 22 de Janeiro de 2023 : Domingo, 22 de Janeiro de 2023 : Domingo, 22 de Janeiro de 2023 : Domingo, 22 de Janeiro de 2023 : Domingo, 22 de Janeiro de 2023 : Domingo, 22 de Janeiro de 2023 : Domingo, 22 de Janeiro de 2023 : Domingo, 22 de Janeiro de 2023 : Domingo, 22 de Janeiro de 2023 : Domingo, 22 de Janeiro de 2023 : ', '30', '150', '450'),
-(33, 'Joao', '48299289867', 'Adão Negro', 'Rua Paulito Nunes N89', 'joao@joao', '1990-02-22', 'aluno', 'segunda-feira: 8h<br> <br> <br> ', '123', '[Data da Matrícula: Domingo 22 de Janeiro de 2023] | Domingo, 22 de Janeiro de 2023 : 0001 | Domingo, 22 de Janeiro de 2023 : 0002 | Domingo, 22 de Janeiro de 2023 : 003 | Domingo, 22 de Janeiro de 2023 : 004 | Domingo, 22 de Janeiro de 2023 : 005 | Domingo, 22 de Janeiro de 2023 : 006 | Domingo, 22 de Janeiro de 2023 : 007 | Domingo, 22 de Janeiro de 2023 : 008 | Domingo, 22 de Janeiro de 2023 : 0009 | Domingo, 22 de Janeiro de 2023 :  | Domingo, 22 de Janeiro de 2023 :  | Domingo, 22 de Janeiro de 2023 :  | <p>Domingo, 22 de Janeiro de 2023 : <br>00099<p>', 'Domingo 22 de Janeiro de 2023', 'Pagou: R$:140Domingo, 22 de Janeiro de 2023 : Domingo, 22 de Janeiro de 2023 : ', 'Pagamento: Domingo, 22 de Janeiro de 2023 : Domingo, 22 de Janeiro de 2023 : Domingo, 22 de Janeiro de 2023 : Domingo, 22 de Janeiro de 2023 : Domingo, 22 de Janeiro de 2023 : Domingo, 22 de Janeiro de 2023 : Domingo, 22 de Janeiro de 2023 : Domingo, 22 de Janeiro de 2023 : ', '35', '35', '420'),
-(34, 'Eduarda', '31908098', 'Raulinho', 'Rua Edivaldo', 'duda@duda', '1258-01-11', 'aluno', 'quinta-feira: 17h<br> <br> <br> ', '123', '[Data da Matrícula: Domingo 22 de Janeiro de 2023] | Domingo, 22 de Janeiro de 2023 : 0001 | Domingo, 22 de Janeiro de 2023 : 002 | <p>Domingo, 22 de Janeiro de 2023 : <br>0003<p>', 'Domingo 22 de Janeiro de 2023', 'Pagou: R$:70Domingo, 22 de Janeiro de 2023 : ', 'Pagamento: Domingo, 22 de Janeiro de 2023 : Domingo, 22 de Janeiro de 2023 : ', '35', '0', '105');
+INSERT INTO `usuarios` (`id`, `nome`, `fone`, `responsavel`, `endereco`, `email`, `data_nasc`, `tipo`, `agenda`, `senha`, `aulas`, `matricula`, `financeiro_aberto`, `financeiro_pago`, `valor_aula`, `divida_ativa`, `divida_paga`, `reg_aula`) VALUES
+(1, 'Jonathas Martinelli Peralta', '51981717498', 'Jonathas', 'Rua Capitao Cruz', 'mano@mano', '0000-00-00', '', 'terça-feira: 14h<br><br><br>', '123', '. | Quarta-Feira, 25 de Janeiro de 2023 : aberto | Quinta-Feira, 26 de Janeiro de 2023 : 0000001 | Quinta-Feira, 26 de Janeiro de 2023 : 0002222 | Quinta-Feira, 26 de Janeiro de 2023 : 0003333 | Quinta-Feira, 2 de Fevereiro de 2023 : ljkknlxcfg | Quinta-Feira, 2 de Fevereiro de 2023 : sdfgfdfg | <p>Sexta-Feira, 3 de Fevereiro de 2023 : <br>Letras móveis <p>', '0', 'Pagou: R$:50Sexta-Feira, 3 de Fevereiro de 2023 : ', ' Quarta-Feira, 25 de Janeiro de 2023 : 40Quinta-Feira, 26 de Janeiro de 2023 : 120Quinta-Feira, 26 de Janeiro de 2023 : 160Quinta-Feira, 26 de Janeiro de 2023 : 160Quinta-Feira, 26 de Janeiro de 2023 : 215Quinta-Feira, 2 de Fevereiro de 2023 : 240Sexta-Feira, 3 de Fevereiro de 2023 : 290', '40', '-10', '290', 'Registro da aula teste whatsapp'),
+(37, 'Cristiana Regina Modzelan', '51983330238', 'Cristiana', 'Rua Capitao Cruz, 2801', 'cris@cris', '1990-08-19', 'professor', 'segunda-feira: 8h<br><br><br>', '123', '[Data da Matrícula: Sexta-Feira 3 de Fevereiro de 2023] | Sexta-Feira, 3 de Fevereiro de 2023 : Teste de registro e envio para whatsapp | Sexta-Feira, 3 de Fevereiro de 2023 : TESTE DE REGISTRO DO WHATSAPP AULA 002 | <p>Quarta-Feira, 22 de Fevereiro de 2023 : <br>merda<p>', 'Sexta-Feira 3 de Fevereiro de 2023', 'Pagou: R$:35Quarta-Feira, 22 de Fevereiro de 2023 : ', '0Sábado, 4 de Fevereiro de 2023 : 105Quarta-Feira, 22 de Fevereiro de 2023 : 140', '35', '0', '140', 'merda');
 
 --
 -- Índices para tabelas despejadas
@@ -75,7 +74,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
